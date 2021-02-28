ea12d1bf43fdef93bb8602056594438396fc352b
DaeseongKim <eotjd1105@icloud.com>
2021-02-28 15:57:25 +0900

    MAKE ball

---
+++ app.js
@@ -0,0 +1,34 @@
+import { Ball } from "./ball.js";
+
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+
+    document.body.appendChild(this.canvas);
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    this.ball = new Ball(this.stageWidth, this.stageHeight, 45, 5);
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
+  }
+  animate(t) {
+    window.requestAnimationFrame(this.animate.bind(this));
+
+    this.ball.draw(this.ctx, this.stageWidth, this.stageHeight);
+  }
+}
+
+window.onload = function () {
+  new App();
+};
